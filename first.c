846cea6fd11c530d79ea8c95697de3faf634ca78
Faouzi-k <faouzi.khelifa@yahoo.com>
2022-11-08 17:28:55 +0100

    ajouter des variables

--- first.c
+++ first.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>  //Dirctives de préprocesseur
+#include <stdbool.h>
 
-int main()
+int main()           // Fonction
 {
-	printf("Bonjour Gérard\n");
+	/*
+    int nobmreDevies = 5, niveau = 1 ;
+
+    printf ("Vous avez %d vies et vous etes au niveau N° %d \n" ,nobmreDevies,niveau);
+    */
+
+    int userAge = 0; //On initialise la variable à 0
+    printf ("Quel age avez-vous ?\n");
+    scanf ("%d",&userAge); //On demande d'entrer l'age avec scanf
+    printf("Ah ! Vous avez donc %d ans !\n",userAge);
+    
+    
+
 	return 0;
 }
